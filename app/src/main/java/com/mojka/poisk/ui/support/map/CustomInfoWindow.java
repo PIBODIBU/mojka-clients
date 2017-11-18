fdc8ea25f3b8b1bc3690253868fdbba4123ec257
Nakamoto <romeo97934@gmail.com>
2017-11-19 00:22:05 +0200

    Map marker info window bug fixed

--- app/src/main/java/com/mojka/poisk/ui/support/map/CustomInfoWindow.java
+++ app/src/main/java/com/mojka/poisk/ui/support/map/CustomInfoWindow.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 import com.mojka.poisk.R;
